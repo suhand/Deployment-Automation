0dc9a861194304aec0d253f435fd9bbbc519fa76
Suhan Dharmasuriya <suhanr@wso2.com>
2014-11-26 17:39:28 +0530

    secondary scripts commit
    committing python scripts

---
+++ deployment automation with xml config/python_scripts/deploy_script.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+from novaclient.v1_1 import client
+import os
+import time
+import libxml2
+from subprocess import call
+
+# Retrieve authentication credentials from OS environmental variables
+# These are defined at /root/.bash_profile
+# Current test bed used is Cloud Support Labs/Tenant name: Stratos
+def get_nova_creds():
+	return { 'username': os.environ['OS_USERNAME'],
+                 'api_key': os.environ['OS_PASSWORD'],
+                 'auth_url': os.environ['OS_AUTH_URL'],
+                 'project_id': os.environ['OS_TENANT_NAME']
+               }
+
+# Main body of the script
+if __name__ == '__main__':
+    try:
+
+	i=0
+	instanceList=[]
+
+	#this dictionary contains the IP addresses of the populated instances
+	ipmap = {}
+        
+	# Get the configurations details of the deployment
+	doc = libxml2.parseFile('conf.xml')
+
+        # Load credentials
+        creds = get_nova_creds()
+        nova = client.Client(**creds)
+	print("Generating nova credentials...")
+	#nova.authenticate()
+
+        # This is where server image, flavor and network information is retrieved
+	print("Retrieving image, flavor and network information...")
+
+	# Find image= "ubuntu-12.04-automated" 
+        image=nova.images.find(name="suhan-daf-agent2-ubuntu14.04")
+	#image=nova.images.find(name="Ubuntu Cloud 12.04")
+	print image
+        
+	# Find flavor for 2GB RAM
+	flavor=nova.flavors.find(name="m3.tiny")
+	print flavor
+	
+	# Since multiple networks find network-routable network
+	# This enables ssh capabilities to the instance created
+        network=nova.networks.find(label="net0")
+	# Retrieve the newtork id string from the network object we created
+        nics = [{'net-id': network.id}]
+	print network
+	#print nics
+
+	for node in doc.xpathEval("//node"):
+                server = nova.servers.create(name = node.prop('id'),password="suhan",image= image.id,flavor= flavor.id ,key_name = "mycloudkey",nics =[{'net-id': network.id ,'v4-fixed-ip': ''}])
+                instanceList.append(server)
+		print server.id
+                time.sleep(40)
+                print instanceList[i].status
+                #floating_ip = nova.floating_ips.create(nova.floating_ip_pools.list()[0].name)
+                #print floating_ip
+		#server.add_floating_ip(floating_ip)
+		print instanceList[i].addresses
+                ipmap[node.prop('id')] =  (((instanceList[i].addresses)['net0'])[0])['addr']
+                print ipmap[node.prop('id')]
+		print ipmap
+		instanceID=instanceList[i].id
+                instanceList[i].suspend()
+		#print instanceList[i].status
+                i=i+1
+	time.sleep(5)
+        j=0;
+	print "---- end of instance spawning ----"
+	
+	while(j<len(doc.xpathEval("//node"))):
+		print len(doc.xpathEval("//node"))
+        	currentNode= doc.xpathEval("//node")[j]
+        	configFilepath = '/tmp/'+ ipmap[currentNode.prop('id')]
+		xmlstring = currentNode.serialize('UTF-8', 1).replace('\n', '')
+		print xmlstring
+        	t = xmlstring.format(**ipmap)
+        	f = open(configFilepath,'w+')
+        	f.write('configurations,'+ t)
+        	f.close()
+        	instanceList[j].resume()
+		time.sleep(5)
+		instanceList[j].reboot()
+		#call("while ! echo exit | nc "+ipmap[currentNode.prop('id')]+" 9443; do sleep 10; done", shell="True")	
+		#print "Server "+ipmap[currentNode.prop('id')]+" is running now"
+		j=j+1
+	
+    except BaseException as b:
+        print 'exception is ', b
+
---
+++ deployment automation with xml config/python_scripts/new_instance.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from novaclient.v1_1 import client
+import os
+from subprocess import call
+
+# Retrieve authentication credentials from OS environmental variables
+# These are defined at /root/.bash_profile
+# Current test bed used is Cloud Support Labs/Tenant name: Stratos
+def get_nova_creds():
+	return { 'username': os.environ['OS_USERNAME'],
+                 'api_key': os.environ['OS_PASSWORD'],
+                 'auth_url': os.environ['OS_AUTH_URL'],
+                 'project_id': os.environ['OS_TENANT_NAME']
+               }
+
+# Main body of the script
+if __name__ == '__main__':
+    try:
+        creds = get_nova_creds()
+        nova = client.Client(**creds)
+	print("Generating nova credentials...")
+	#nova.authenticate()
+
+        # This is where server image, flavor and network information is retrieved
+	print("Retrieving image, flavor and network information...")
+
+	# Find image= "ubuntu-12.04-automated" 
+        image=nova.images.find(name="suhan-daf-agent-ubuntu12.04")
+	#image=nova.images.find(name="Ubuntu Cloud 12.04")
+	print image
+        
+	# Find flavor for 2GB RAM
+	#flavor=nova.flavors.find(name="m1.medium")
+	flavor=nova.flavors.find(name="m1.tiny")
+	print flavor
+	
+	# Since multiple networks find network-routable network
+	# This enables ssh capabilities to the instance created
+        network=nova.networks.find(label="net0")
+	# Retrieve the newtork id string from the network object we created
+        nics = [{'net-id': network.id}]
+	print network
+	#print nics
+
+        server = nova.servers.create(name = "daf-agent-ganglia-ubuntu12.04",password="suhan",image= image.id,flavor= flavor.id ,key_name = "mycloudkey",nics =[{'net-id': network.id ,'v4-fixed-ip': ''}])
+	print "---- OpenStack instance successfully created ----"
+	
+    except BaseException as b:
+        print 'exception is ', b
+
