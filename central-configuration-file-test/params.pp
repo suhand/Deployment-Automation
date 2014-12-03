6ee68f8fe0bd38e0744020cd41f5ed009b6ca710
Suhan Dharmasuriya <suhand@users.noreply.github.com>
2014-12-03 10:46:07 +0530

    tab removed

--- central-configuration-file-test/params.pp
+++ central-configuration-file-test/params.pp
@@ -2,5 +2,5 @@ class appserver::params ($say_hello_to = 'guys and gals') {
         $myname = 'welcome file.xml'
         $serverOptions     = '-DworkerNode=true'
         $subDomain         = 'worker'
-		$servers           = { '192.168.1.156' => '4100', '192.168.1.157' => '4000' }
+	$servers           = { '192.168.1.156' => '4100', '192.168.1.157' => '4000' }
 }
