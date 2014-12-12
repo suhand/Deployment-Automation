918ad664bcdd2e1bb598ff51c09319cacd9cc0e2
Suhan Dharmasuriya <suhanr@wso2.com>
2014-12-12 17:41:27 +0530

    site.pp test node definitions removed

--- deployment-automation-total-puppet/puppet/manifests/site.pp
+++ deployment-automation-total-puppet/puppet/manifests/site.pp
@@ -3,24 +3,3 @@ import 'appserver'
 node 'default' {
 	include appserver
 }
-
-node 'appserver-wkr' {
-	# params.pp -> init.pp (class appserver)
-	include appserver
-}
-
-node 'test-node' {
-
-	#this is a test
-	$say_hello_to = 'guys and gals'
-        $myname = 'welcome file.xml'
-	$serverOptions     = '-DworkerNode=true'
-        $subDomain         = 'worker'
-        $members           = ['10.0.1.196', '10.0.1.198', '10.0.1.200', '10.0.1.202']
-
-        file {  "/tmp/$myname":
-                ensure  => file,
-                content => template('appserver/polite-file.erb'),
-        }
-
-}
