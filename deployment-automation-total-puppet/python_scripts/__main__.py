557cee4dfbb7506aa351fc0adf5c8788e1569f46
Suhan Dharmasuriya <suhanr@wso2.com>
2014-12-15 15:30:02 +0530

    New configuration file added
    deployment.cfg

--- deployment-automation-total-puppet/python_scripts/__main__.py
+++ deployment-automation-total-puppet/python_scripts/__main__.py
@@ -1,24 +1,39 @@
 import os
 from novaclient.v1_1 import client
 #import ec2
-from openstack import initialize_cluster
+from load_deployment_config import get_environment
+from load_deployment_config import get_openstack_image
+from load_deployment_config import get_openstack_flavor
+from load_deployment_config import get_openstack_network
+from load_deployment_config import get_openstack_instancePassword
+from load_deployment_config import get_openstack_keyPair
 from load_deployment_config import load_server_config
+from openstack import initialize_cluster
 
 if __name__ == '__main__':
     try:
-	# load deployment configurations
-	print (" Loading deployment cluster configuration...")
-	serverList = load_server_config()
-	
-	# load nova configs and create instances
-	print (" Loading nova configuration and spawning instances...")
-	imageName = "suhan-daf-agentv4-ubuntu14.04"
-	flavorName = "m2.small"
-	networkName = "qaa-net"
-	instancePassword = "suhan"
-	keyPairName = "mycloudkey"
-	
-	cluster = initialize_cluster(serverList, imageName, flavorName, networkName, instancePassword, keyPairName)
+	# get deployment automation environment name
+	print (" Finding Environment...")
+        environment = get_environment()
+
+	if environment == "openstack":
+
+		# load openstack cloud configurations
+        	print (" Loading nova configuration and spawning instances...")
+        	imageName = get_openstack_image()
+        	flavorName = get_openstack_flavor()
+        	networkName = get_openstack_network()
+        	instancePassword = get_openstack_instancePassword()
+        	keyPairName = get_openstack_keyPair()
+
+		# load server configurations
+		print (" Loading deployment cluster configuration...")
+		serverList = load_server_config()
+
+		cluster = initialize_cluster(serverList, imageName, flavorName, networkName, instancePassword, keyPairName)
+
+	else:
+		print (" Deployment automation for " + environment + " is not implemented yet...")
 
     except BaseException as b:
         print 'Exception in __main__.py: ', b
--- deployment-automation-total-puppet/python_scripts/load_deployment_config.py
+++ deployment-automation-total-puppet/python_scripts/load_deployment_config.py
@@ -1,18 +1,54 @@
 #import os
 #import time
 #from novaclient.v1_1 import client
+import ConfigParser
+
+# Global variables
+config = ConfigParser.RawConfigParser(allow_no_value=True)
+
+# Get environment
+def get_environment():
+	config.read('deployment.cfg')
+	return config.get('environment', 'env')
+
+# Load environment configuration
+def get_openstack_image():
+	config.read('deployment.cfg')
+	return config.get('envconfig', 'image')
+
+def get_openstack_flavor():
+	config.read('deployment.cfg')
+	return config.get('envconfig', 'flavor')
+
+def get_openstack_network():
+	config.read('deployment.cfg')
+	return config.get('envconfig', 'network')
+
+def get_openstack_instancePassword():
+	config.read('deployment.cfg')
+	return config.get('envconfig', 'instancePassword')
+
+def get_openstack_keyPair():
+	config.read('deployment.cfg')
+	return config.get('envconfig', 'keyPair')
 
 # Nova credentials are loaded from OS environmental variables
 def load_server_config():
+	
+	serverList = []
+	
+	config.read('deployment.cfg')
+	
+	dic = dict(config.items('nodes'))
+	
+	for node, ip in dic.iteritems():
+    		serverList.append(node)
 
-	with open('nodes.txt', 'r') as f:
-    		cluster = f.readlines()
-
-    		for line in cluster:
-			serverList = line.split()
-        		print serverList
-			#serverList.append(words)
+	print serverList
+	serverList.sort()
+	print serverList
 	return serverList
+
 # This block will only get executed when running directly
 # This can be used to debug given nova client credentials and authentication
 if __name__ == '__main__':
