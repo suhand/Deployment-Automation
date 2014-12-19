60260726e2e7e354d2945864462333c42165e6a8
Suhan Dharmasuriya <suhanr@wso2.com>
2014-12-19 15:30:38 +0530

    generalised version, path, pack names

--- deployment-automation-total-puppet/puppet/modules/appserver/manifests/init.pp
+++ deployment-automation-total-puppet/puppet/modules/appserver/manifests/init.pp
@@ -3,23 +3,23 @@ class appserver inherits appserver::params {
         $serverName = $::hostname
 	$setServerOption = $servers[$serverName][serverOptions]
 
-	define fill_templates($axis2, $carbon, $clustering, $serverOptions) {
+	define fill_templates($axis2, $carbon, $clustering, $serverOptions, $server_home) {
         	$ipAdd = $::ipaddress
         	$hostName = $::hostname
 		notify{"hostName -> $hostName, arrayName -> ${name}, serverOptions -> $serverOptions":}
         	if $hostName == "${name}" {
                 	notify {"host name match found for $hostName for $ipAdd":}
-                	file {  "/opt/wso2as-5.2.1/repository/conf/axis2/axis2.xml":
+                	file {  "${server_home}/repository/conf/axis2/axis2.xml":
                         	ensure  => file,
                         	content => template('appserver/axis2.xml.erb'),
                 	}
                 	->
-                	file {  "/opt/wso2as-5.2.1/repository/conf/carbon.xml":
+                	file {  "${server_home}/repository/conf/carbon.xml":
                         	ensure  => file,
                         	content => template('appserver/carbon.xml.erb'),
                 	}
                 	->
-                	file {  "/opt/wso2as-5.2.1/repository/conf/tomcat/catalina-server.xml":
+                	file {  "${server_home}/repository/conf/tomcat/catalina-server.xml":
                         	ensure  => file,
                         	content => template('appserver/catalina-server.xml.erb'),
                 	}
@@ -32,57 +32,51 @@ class appserver inherits appserver::params {
 
         exec {  "Stop_process_and_remove_CARBON_HOME":
                 path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-                command => "kill -9 `cat /opt/wso2as-5.2.1/wso2carbon.pid` ; rm -rf /opt/wso2as-5.2.1";
+                command => "kill -9 `cat ${carbon_home}/wso2carbon.pid` ; rm -rf ${carbon_home}";
         }
         ->
         exec {  "remove_java_dirs":
                 path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-                command => "rm -rf /opt/jdk1.6.0_24 ; rm -rf /opt/java";
+                command => "rm -rf ${target}/${jdk_version} ; rm -rf /opt/java";
         }
         ->
-        file { '/opt/wso2as-5.2.1.zip':
+        file { "${pack_file}":
                 replace => "no",
                 ensure => present,
-                source => "puppet:///modules/appserver/wso2as-5.2.1.zip",
+                source => "puppet:///modules/appserver/${carbon_version}.zip",
         }
-
-        file { '/opt/jdk1.6.0_24.tar.gz':
+	->
+        file { "${jdk_file}":
                 replace => "no",
                 ensure => present,
-                source => "puppet:///modules/packs/jdk1.6.0_24.tar.gz",
+                source => "puppet:///modules/packs/${jdk_version}.tar.gz",
         }
-
+	->
         exec {  "installing_java":
                 user => 'root',
                 path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-                command => "tar -xvzf /opt/jdk1.6.0_24.tar.gz  -C /opt/; ln -s /opt/jdk1.6.0_24 /opt/java",
+                command => "tar -xvzf ${target}/${jdk_version}.tar.gz  -C ${target}/; ln -s ${target}/${jdk_version} /opt/java",
                 creates => "/tmp/installjava.log",
-                require   => File["/opt/jdk1.6.0_24.tar.gz"];
+                require   => File["${jdk_file}"];
 
                 "extracting appserver":
                 user => 'root',
                 path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/java/bin/',
-                command => "unzip /opt/wso2as-5.2.1.zip -d /opt/",
+                command => "unzip ${carbon_home}.zip -d ${target}/",
                 require   => Exec["installing_java"];
-        }
-
-        exec {  'wait_till_product_pack_extracts' :
-                require => Exec["extracting appserver"],
-                command => "sleep 10",
-                path => "/usr/bin:/bin",
         }
 
 	create_resources(appserver::fill_templates, $servers, $serversDefaults)
 
-	Exec<| title == "wait_till_product_pack_extracts" |> -> Appserver::Fill_Templates<| |> -> Exec<| title == "starting_appserver" |>
+	Exec<| title == "extracting appserver" |> -> Appserver::Fill_Templates<| |> -> Exec<| title == "starting_appserver" |>
 
 	exec {  "starting_appserver":
                 user    => 'root',
                 environment => "JAVA_HOME=/opt/java",
                 path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/java/bin/',
-                unless  => "test -f /opt/wso2as-5.2.1/wso2carbon.lck",
-                command => "touch /opt/wso2as-5.2.1/wso2carbon.lck; /opt/wso2as-5.2.1/bin/wso2server.sh  $setServerOption > /dev/null 2>&1 &",
-                creates => "/opt/wso2as-5.2.1/repository/wso2carbon.log",
-                require   => Exec["wait_till_product_pack_extracts"];
+                unless  => "test -f ${carbon_home}/wso2carbon.lck",
+                command => "touch ${carbon_home}/wso2carbon.lck; ${carbon_home}/bin/wso2server.sh  $setServerOption > /dev/null 2>&1 &",
+                creates => "${carbon_home}/repository/wso2carbon.log",
+		require => Exec["extracting appserver"];
         }
 }
--- deployment-automation-total-puppet/python_scripts/config.pp
+++ deployment-automation-total-puppet/python_scripts/config.pp
@@ -1,11 +1,39 @@
 class appserver::params {
+	
+	$owner  = 'root'
+  	$group  = 'root'
+  	$target = '/opt'
+
+	$deployment_code = 'as'
+	$service_code   = 'as'
+	$version = '5.2.1'
+	$offset = 0
+  	$carbon_version = "wso2${service_code}-${version}"
+  	$carbon_home    = "${target}/${carbon_version}"
+	
+	$hazelcast_port = 4100
+        $elb_common_port = 4500
+        $elb_mgt_cluster_port = 4500
+        $elb_wkr_cluster_port = 4600
+
+	# Available JDK versions
+	# jdk1.6.0_24
+	# jdk1.7.0_51
+	$jdk_version = 'jdk1.6.0_24'
+	$jdk_home = "${target}/${jdk_version}"	
+
+	$jdk_file	= "${jdk_home}.tar.gz"
+	$pack_file	= "${carbon_home}.zip"
+
 	$servers = {
                 appserver-mgr2 => { axis2   => {subDomain => 'mgt',},
                                     carbon  => {subDomain => 'mgt',},
-				    serverOptions => '-Dsetup',  },
+				    serverOptions => '-Dsetup', 
+				    server_home => $carbon_home, },
                 appserver-wkr2 => { axis2   => {subDomain => 'worker', members => ['appserver-mgr2-ip']},
                                     carbon => {subDomain => 'worker',},
-				    serverOptions => '-DworkerNode=true',  },
+				    serverOptions => '-DworkerNode=true',
+				    server_home => $carbon_home, },
         }
 
         $serversDefaults = {
